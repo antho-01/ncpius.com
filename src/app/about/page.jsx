64805a53603a3ef16139a8666430711c2e43d263
anthony david tom <anthodave01@gmail.com>
2024-08-29 06:22:20 +0100

    test10

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import styles from "@/app/about/page.module.css"
 const About = () => {
       
--- src/app/homelisting/page.jsx
+++ src/app/homelisting/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import styles from "@/app/homelisting/page.module.css"
 import { IoBedOutline } from "react-icons/io5";
